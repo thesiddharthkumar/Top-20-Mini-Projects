78ea723c434751587d8a472ef27f994c08aa68e1
Siddharth K <121507119+thesiddharthkumar@users.noreply.github.com>
2025-10-11 20:43:10 +0530

    Add files via upload

---
+++ Typezone/scripts.js
@@ -0,0 +1,17 @@
+const themeSelectorShow = document.getElementById('theme-selector-show');
+const themeDropdown = document.getElementById('theme-dropdown');
+const currentTheme = document.getElementById('current-theme');
+const themeOptions = document.querySelectorAll('.theme-option');
+const darkModeToggle = document.getElementById('dark-mode-toggle');
+const celebrationContainer = document.getElementById('celebration-container');
+const finalWpmDisplay = document.getElementById('final-wpm');
+const finalAccuracyDisplay = document.getElementById('final-accuracy');
+const closeCelebrationBtn = document.getElementById('close-celebration');
+const confettiPiecesContainer = document.getElementById('confetti-pieces-container');
+
+//   === Starting ===
+
+// Initialize current theme
+
+
+//  ==== END ====
