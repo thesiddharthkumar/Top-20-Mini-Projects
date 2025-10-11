6ecf4fd861d6e90f5f51fb05ff7d4b535c7811ea
Siddharth K <121507119+thesiddharthkumar@users.noreply.github.com>
2025-10-11 20:49:48 +0530

    Add files via upload

---
+++ User Authentication With Google/script.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const showSignup = document.getElementById('show-signup');
+    const showLogin = document.getElementById('show-login');
+    const authContainer = document.querySelector('.auth-container');
+    const loginContainer = document.querySelector('.login');
+    const signupContainer = document.querySelector('.signup');
+    const loginButton = document.getElementById('login');
+    const signupButton = document.getElementById('signup');
+    const googleLoginButton = document.getElementById('google-login');
+    const googleSignupButton = document.getElementById('google-signup');
+    const notificationContainer = document.getElementById('notification-container');
+
+    const loginEmail = document.getElementById('login-email');
+    const loginPassword = document.getElementById('login-password');
+    const signupEmail = document.getElementById('signup-email');
+    const signupPassword = document.getElementById('signup-password');
+    const confirmPassword = document.getElementById('confirm-password');
+
+
+    // Starting..
+})
