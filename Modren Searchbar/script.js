f218c145255a372cb9ceb9b7410b8b0dd883f474
Siddharth K <121507119+thesiddharthkumar@users.noreply.github.com>
2025-10-29 22:16:57 +0530

    Add files via upload

---
+++ Modren Searchbar/script.js
@@ -0,0 +1,14 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const searchBox = document.querySelector('.search-box');
+    const searchBtn = document.querySelector('.search-btn');
+    const clearBtn = document.querySelector('.clear-btn');
+    const searchResults = document.querySelector('.search-results');
+    const searchIcon = searchBtn.querySelector('i');
+    const resultItems = document.querySelectorAll('.result-item');
+    const cardPopup = document.querySelector('.card-popup');
+    const closeBtn = document.querySelector('.close-btn');
+
+    // =============== Starting Code ... =============== 
+
+
+})
